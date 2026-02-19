8f8f1db7af8582b85b7833c10b451e75f556035e
Santiago cussi <perejilinformatico@gmail.com>
2026-02-19 19:33:12 +0100

    Cambios, carrito en funcinamiento mejor ui

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ const carritoHTML = document.getElementsByClassName("carrito__html")[0];
 const currentHash = window.location.hash;
 const cards = document.querySelectorAll(".card__div");
 const ulList = document.querySelector("#ul_list");
+const btnAñadirCarrito = document.getElementsByClassName("visualizer__card")[0];
+const p__textVisualizerCard = document.getElementsByClassName("p__textVisualizerCard")[0];
 const li = document.createElement("li")
+const btn__principalSali = document.getElementsByClassName("btn__principalSali")[0];
+const btn__AñadirCarrito = document.querySelectorAll(".btn__AñadirCarrito");
+const p__textReutilizable = document.querySelector(".p__textReutilizable")
 
 i.addEventListener("click", () => {
   window.location.hash = "#jsjs";
@@ -27,7 +32,9 @@ cards.forEach(element => {
         for (let i = 0; i < cards.length; i++) {
           if (cards[i] === element) {
             cards[i].style.border = "2px solid var(--gold)";
-            console
+            console.log(cards[i].getAttribute("data-category"));
+            btnAñadirCarrito.style.display = "block";
+            p__textVisualizerCard.textContent = cards[i].getAttribute("data-category")
           } else {
             cards[i].style.border = "none";
           }
@@ -35,13 +42,16 @@ cards.forEach(element => {
     });
 });
 
-cards.forEach(element_card => {
-    element_card.addEventListener("click", () => {
-        document.getElementsByClassName("p__textReutilizable")[0].style.display = "none";
-        console.log(element_card.getAttribute("data-category"));
-        const li = document.createElement("li")
-        li.textContent = element_card.getAttribute("data-category");
-        ulList.appendChild(li)
+btn__AñadirCarrito.forEach(element__btnañadir => {
+    element__btnañadir.addEventListener("click", () => {
+      btnAñadirCarrito.style.display = "none";
+      const li = document.createElement("li");
+      li.textContent = p__textVisualizerCard.textContent;
+      ulList.appendChild(li);
+      p__textReutilizable.style.display = "none";
     });
 });
 
+btn__principalSali.addEventListener("click", () => {
+    btnAñadirCarrito.style.display = "none";
+});
